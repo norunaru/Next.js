585503b2430a476bd340b6ddd1036987b9e4352a
gitlab 이름 <nwy98@naver.com>
2025-02-12 15:43:43 +0900

    250212

---
+++ forum/app/detail/[id]/not-found.js
@@ -0,0 +1,3 @@
+export default function NotFound() {
+  return <div>없는페이지</div>;
+}
--- forum/app/detail/[id]/page.js
+++ forum/app/detail/[id]/page.js
@@ -1,6 +1,7 @@
 import { connectDB } from "@/util/database";
 import { ObjectId } from "mongodb";
 import Comment from "./Comment";
+import { notFound } from "next/navigation";
 
 export default async function Detail(props) {
   const db = (await connectDB).db("forum");
@@ -8,10 +9,14 @@ export default async function Detail(props) {
     .collection("post")
     .findOne({ _id: new ObjectId(props.params.id) });
   console.log(props.params.id);
+
+  if (result == null) {
+    return notFound();
+  }
   return (
     <div>
       <h4>상세페이지</h4>
-      <h4>{result.title}</h4>
+      <h4>{result.title.a.sadf}</h4>
       <p>{result.content}</p>
       <Comment id={props.params.id} />
     </div>
